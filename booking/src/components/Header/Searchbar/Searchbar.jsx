ed8e530136ce2090fa4d95ad88f1c53693c4879f
MilaKropeczka <milakropeczka@gmail.com>
2024-09-11 12:55:54 +0200

    feat: add search bat to header

---
+++ booking/src/components/Header/Searchbar/Searchbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Searchbar() {
+	return (
+		<div className='d-flex'>
+			<input
+				className='form-control'
+				type='text'
+				placeholder='Szukaj...'
+			/>
+			<button className='btn btn-primary'>Szukaj</button>
+		</div>
+	);
+}
