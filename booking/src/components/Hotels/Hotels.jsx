aa7458a401fc2906fabd57326712035faeb78611
MilaKropeczka <milakropeczka@gmail.com>
2024-09-12 13:08:42 +0200

    refactor: update structure layout Hotels.

--- booking/src/components/Hotels/Hotels.jsx
+++ booking/src/components/Hotels/Hotels.jsx
@@ -5,7 +5,7 @@ import style from './Hotels.module.css';
 class Hotels extends Component {
 	render() {
 		return (
-			<div className={style.container}>
+			<div className={`${style.container} container`}>
 				<h2 className={style.title}>Oferty: </h2>
 				<Hotel />
 				<Hotel />
