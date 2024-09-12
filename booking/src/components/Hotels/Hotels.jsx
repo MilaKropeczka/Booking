f8d68dfb34abe9faefb3b5bf6475ce0cbbc1d32e
MilaKropeczka <milakropeczka@gmail.com>
2024-09-12 20:51:01 +0200

    feat: map list of hotels in component, chore: add PropTypes for component Hotels

--- booking/src/components/Hotels/Hotels.jsx
+++ booking/src/components/Hotels/Hotels.jsx
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import Hotel from './Hotel/Hotel';
 import style from './Hotels.module.css';
+
+const propTypes = {
+	hotels: PropTypes.array.isRequired,
+};
 
 class Hotels extends Component {
 	render() {
 		return (
 			<div className={`${style.container} container`}>
 				<h2 className={style.title}>Oferty: </h2>
-				<Hotel />
-				<Hotel />
+				{this.props.hotels.map((hotel) => (
+					<Hotel key={hotel.id} {...hotel} />
+				))}
 			</div>
 		);
 	}
 }
+
+Hotels.propTypes = propTypes;
 
 export default Hotels;
