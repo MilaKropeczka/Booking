f73955178ef9053bc82a7e241bcf448f2ed100d2
MilaKropeczka <milakropeczka@gmail.com>
2024-09-13 20:54:08 +0200

    feat: add theme and authentication state, context and components (Searchbar, Layout, Footer)

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -4,6 +4,12 @@ import Header from './components/Header/Header';
 import Hotels from './components/Hotels/Hotels';
 import Menu from './components/Menu/Menu';
 import LoadingIcon from './components/UI/LoadingIcon/LoadingIcon';
+import Searchbar from './components/UI/Searchbar/Searchbar';
+import Layout from './components/Layout/Layout';
+import Footer from './components/Footer/Footer';
+import ThemeButton from './components/UI/ThemeButton/ThemeButton';
+import ThemeContext from './components/context/themeContext';
+import AuthContext from './components/context/authContext';
 
 class App extends Component {
 	hotels = [
@@ -29,6 +35,8 @@ class App extends Component {
 	state = {
 		hotels: [],
 		loading: true,
+		theme: 'danger',
+		isAuthenticated: false,
 	};
 
 	searchHandler(term) {
@@ -41,21 +49,50 @@ class App extends Component {
 	componentDidMount() {
 		setTimeout(() => {
 			this.setState({ hotels: this.hotels, loading: false });
-			console.log(`component zamontowany`);
 		}, 1000);
 	}
 
+	changeTheme = () => {
+		const newTheme = this.state.theme === 'primary' ? 'danger' : 'primary';
+		this.setState({ theme: newTheme });
+	};
+
 	render() {
+		const header = (
+			<Header>
+				<Searchbar onSearch={(term) => this.searchHandler(term)} />
+				<ThemeButton />
+			</Header>
+		);
+		const content = this.state.loading ? (
+			<LoadingIcon theme={this.changeTheme} />
+		) : (
+			<Hotels hotels={this.state.hotels} />
+		);
+		const menu = <Menu />;
+		const footer = <Footer />;
 		return (
-			<div className='App'>
-				<Header onSearch={(term) => this.searchHandler(term)} />
-				<Menu />
-				{this.state.loading ? (
-					<LoadingIcon />
-				) : (
-					<Hotels hotels={this.state.hotels} />
-				)}
-			</div>
+			<AuthContext.Provider
+				value={{
+					isAuthenticated: this.state.isAuthenticated,
+					login: () => this.setState({ isAuthenticated: true }),
+					logout: () => this.setState({ isAuthenticated: false }),
+				}}>
+				<ThemeContext.Provider
+					value={{
+						color: this.state.theme,
+						changeTheme: this.changeTheme,
+					}}>
+					<div className='App'>
+						<Layout
+							header={header}
+							content={content}
+							menu={menu}
+							footer={footer}
+						/>
+					</div>
+				</ThemeContext.Provider>
+			</AuthContext.Provider>
 		);
 	}
 }
