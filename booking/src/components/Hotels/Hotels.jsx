67f0b91c841c109e21440784335317ab19cf668f
MilaKropeczka <milakropeczka@gmail.com>
2024-09-16 13:56:11 +0200

    feat: add shouldComponentUpdate to optimize hotel refresh

--- booking/src/components/Hotels/Hotels.jsx
+++ booking/src/components/Hotels/Hotels.jsx
@@ -8,6 +8,14 @@ const propTypes = {
 };
 
 class Hotels extends Component {
+	componentDidUpdate() {
+		console.log('odswieza hotele');
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		//żeby hotele nie odswiezały się przy klikaniu w inne przyciski typu theme, login, logout
+		return this.props.hotels === nextProps.hotels ? false : true;
+	}
 	render() {
 		return (
 			<div className={`${style.container}`}>
