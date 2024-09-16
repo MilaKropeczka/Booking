47316a6cf2d62d9e712f7f66cd9164f733ace729
MilaKropeczka <milakropeczka@gmail.com>
2024-09-16 14:16:52 +0200

    refactor: replace PureComponent with memo and convert class component to functional

--- booking/src/components/Hotels/Hotels.jsx
+++ booking/src/components/Hotels/Hotels.jsx
@@ -1,29 +1,27 @@
-import { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Hotel from './Hotel/Hotel';
 import style from './Hotels.module.css';
+import { useEffect } from 'react';
 
 const propTypes = {
 	hotels: PropTypes.array.isRequired,
 };
 
-class Hotels extends PureComponent {
-	componentDidUpdate() {
-		console.log('odswieza hotele');
-	}
-
-	render() {
-		return (
-			<div className={`${style.container}`}>
-				<h2 className={style.title}>Oferty: </h2>
-				{this.props.hotels.map((hotel) => (
-					<Hotel key={hotel.id} {...hotel} />
-				))}
-			</div>
-		);
-	}
+function Hotels(props) {
+	useEffect(() => {
+		console.log('object');
+	}); //calkowite usuniecie drugiego parametru pokazuje odswiezanie komponentu
+	return (
+		<div className={`${style.container}`}>
+			<h2 className={style.title}>Oferty: </h2>
+			{props.hotels.map((hotel) => (
+				<Hotel key={hotel.id} {...hotel} />
+			))}
+		</div>
+	);
 }
 
 Hotels.propTypes = propTypes;
 
-export default Hotels;
+export default React.memo(Hotels);
