b8bb1459e28245dcbaa1c57f080a123a28a0ab60
MilaKropeczka <milakropeczka@gmail.com>
2024-09-22 20:16:48 +0200

    feat: add validations

--- booking/src/helpers/validations.js
+++ booking/src/helpers/validations.js
@@ -2,3 +2,36 @@ export function validateEmail(email) {
 	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return regex.test(email);
 }
+
+//rules
+const availableRules = {
+	required(value) {
+		return value ? '' : 'Pole wymagane';
+	},
+	min(value, rule) {
+		return value.length > rule.length
+			? ''
+			: `Minimum znaków: ${rule.length}`;
+	},
+};
+
+//validation
+export function validate(rules = [], value) {
+	for (let i = 0; i < rules.length; i++) {
+		const rule = rules[i];
+
+		if (rule instanceof Object) {
+			const errorMessage = availableRules[rule.rule](value, rule);
+			if (errorMessage) {
+				return errorMessage;
+			}
+		} else {
+			const errorMessage = availableRules[rule](value);
+			if (errorMessage) {
+				return errorMessage;
+			}
+		}
+	}
+
+	return '';
+}
--- booking/src/pages/Profile/MyHotels/AddHotel/AddHotel.js
+++ booking/src/pages/Profile/MyHotels/AddHotel/AddHotel.js
@@ -1,17 +1,52 @@
 import React, { useRef, useState } from 'react';
 import LoadingButton from '../../../../components/UI/LoadingButton/LoadingButton';
 import Input from '../../../../components/Input/Input';
+import { validate } from '../../../../helpers/validations';
 
 export default function AddHotel() {
 	const imageRef = useRef();
+
 	const [form, setForm] = useState({
-		name: '',
-		description: '',
-		city: '',
-		rooms: 2,
-		features: [],
-		image: null,
-		status: 0,
+		name: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required', { rule: 'min', length: 4 }],
+		},
+		description: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required', { rule: 'min', length: 10 }],
+		},
+		city: {
+			value: '',
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
+		rooms: {
+			value: 2,
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
+		features: {
+			value: [],
+			error: '',
+			showError: false,
+		},
+		image: {
+			value: null,
+			error: '',
+			showError: false,
+		},
+		status: {
+			value: 0,
+			error: '',
+			showError: false,
+			rules: ['required'],
+		},
 	});
 
 	const [loading, setLoading] = useState(false);
@@ -26,6 +61,19 @@ export default function AddHotel() {
 		}, 500);
 	};
 
+	const changeHandler = (value, fieldName) => {
+		const error = validate(form[fieldName].rules, value);
+		setForm({
+			...form,
+			[fieldName]: {
+				...form[fieldName],
+				value,
+				showError: true,
+				error: error,
+			},
+		});
+	};
+
 	return (
 		<div className='card'>
 			<div className='card-header'>Nowy hotel</div>
@@ -34,67 +82,65 @@ export default function AddHotel() {
 				<form onSubmit={sumbit}>
 					<Input
 						label='Nazwa'
-						value={form.name}
-						onChange={(value) => setForm({ ...form, name: value })}
-						error=''
-						showError={false}
+						value={form.name.value}
+						onChange={(value) => changeHandler(value, 'name')}
+						error={form.name.error}
+						showError={form.name.showError}
 					/>
 					<Input
 						label='Opis'
 						type='textarea'
-						value={form.description}
+						value={form.description.value}
 						onChange={(value) =>
-							setForm({ ...form, description: value })
+							changeHandler(value, 'description')
 						}
-						error=''
-						showError={false}
+						error={form.description.error}
+						showError={form.description.showError}
 					/>
 					<Input
 						label='Miejscowość'
-						value={form.city}
-						onChange={(value) => setForm({ ...form, city: value })}
-						error=''
-						showError={false}
+						value={form.city.value}
+						onChange={(value) => changeHandler(value, 'city')}
+						error={form.city.error}
+						showError={form.city.showError}
 					/>
 					<Input
 						label='Ilość pokoi'
 						type='select'
-						value={form.rooms}
-						onChange={(value) => setForm({ ...form, rooms: value })}
+						value={form.rooms.value}
+						onChange={(value) => changeHandler(value, 'rooms')}
 						options={[
 							{ value: 1, label: 1 },
 							{ value: 2, label: 2 },
 							{ value: 3, label: 3 },
 							{ value: 4, label: 4 },
 						]}
-						error=''
-						showError={false}
+						error={form.rooms.error}
+						showError={form.rooms.showError}
 					/>
 					<br />
 
 					<Input
 						label='Udogodnienia'
 						type='checkbox'
-						selectedValues={form.features}
-						onChange={(values) =>
-							setForm({ ...form, features: values })
-						}
+						selectedValues={form.features.value}
+						onChange={(value) => changeHandler(value, 'features')}
 						options={[
 							{ value: 'wifi', label: 'Wi-fi' },
 							{ value: 'tv', label: 'TV' },
 							{ value: 'parking', label: 'Parking' },
 						]}
-						error=''
-						showError={false}
+						error={form.features.error}
+						showError={form.features.showError}
 					/>
 					<br />
 
 					<Input
 						label='Zdjęcie'
 						type='file'
-						onChange={(files) => setForm({ ...form, image: files })}
-						error=''
-						showError={false}
+						onChange={(value) => changeHandler(value, 'image')}
+						error={form.image.error}
+						showError={form.image.showError}
 					/>
 
 					<br />
@@ -102,16 +148,14 @@ export default function AddHotel() {
 					<Input
 						label='Status'
 						type='radio'
-						selectedValue={form.status}
-						onChange={(value) =>
-							setForm({ ...form, status: value })
-						}
+						selectedValue={form.status.value}
+						onChange={(value) => changeHandler(value, 'status')}
 						options={[
 							{ value: '1', label: 'Aktywny' },
 							{ value: '0', label: 'Ukryty' },
 						]}
-						error=''
-						showError={false}
+						error={form.status.error}
+						showError={form.status.showError}
 					/>
 					<br />
 					<div className='text-end col-md-6 col-xl-4'>
